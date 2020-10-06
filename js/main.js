3cf744c7a76c97e4a3917c32ce7e6463b28bc253
danielpienaar <dpienaar001@gmail.com>
2020-10-06 19:48:16 +0200

    Positioned logos and styled login heading

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function ztext() {
+    var ztxt = new Ztextify(".title-logo-heading h1", {
+        depth: "20px",
+        layers: 30,
+        fade: true,
+        direction: "forwards",
+        event: "pointer",
+        eventRotation: "45deg"
+    });
+}
+
 $(document).ready(function() {
     $(".newaccount-button").click(function(){
         $('.login-container').toggleClass('focused');
